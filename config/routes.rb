a6bc5add2c04601f6b28231587b27c5e3587cc76
Chris Veleris <chris.veleris@me.com>
2015-10-06 12:13:00 +0000

    Fixed a bug with conflicting users#create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  # match '/signup',  to: 'users#signup',  via: 'get'  
-  
+  devise_for :users, skip: [:registrations, :sessions]
+  as :user do
+    # devise/registrations
+    get 'signup' => 'devise/registrations#new', :as => :new_user_registration
+    post 'signup' => 'devise/registrations#create', :as => :user_registration
+    get 'users/register/cancel' => 'devise/registrations#cancel', :as => :cancel_user_registration
+    get 'users/register/edit' => 'devise/registrations#edit', :as => :edit_user_registration
+    put 'users/register' => 'devise/registrations#update'
+    delete 'users/register/cancel' => 'devise/registrations#destroy'
+
+    # devise/sessions
+    get 'signin' => 'devise/sessions#new', :as => :new_user_session
+    post 'signin' => 'devise/sessions#create', :as => :user_session
+    get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   get    'dashboard' => 'dashboard#index'
   get    'intro'     => 'welcome#intro'
-  
+
   resources :users
   resources :groups
   resources :ticket_categories
