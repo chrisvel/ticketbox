b6f7a8bfc04de028c6a37dc1195da7f6bb793bec
Chris Veleris <chris.veleris@me.com>
2015-10-07 08:40:27 +0000

    Fixed a wrong symbol

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -21,10 +21,28 @@ feature "Signing in" do
   scenario "Signing in as another user" do
     visit '/signin'
     within("#login-page") do
-      fill_in 'Email', :with => jack.email
-      fill_in 'Password', :with => jack.password
+      fill_in 'Email', with: jack.email
+      fill_in 'Password', with: jack.password
     end
     click_button 'Sign in'
     expect(page).to have_content 'Invalid email or password'
   end
+end
+
+feature "Adding user" do
+  background do
+    @user = FactoryGirl.create :user
+  end
+
+  scenario "Creating user with correct details" do
+    visit '/users/new'
+    within("form") do
+      fill_in 'Email', with: 'someone@here.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+    end
+    click_button 'Save changes'
+    expect(page).to have_content 'Changes saved'
+  end
+
 end
